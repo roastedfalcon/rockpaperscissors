6732224d3069be5180044da1506e84b2e6f1501f
Mark Schlining <maschlining@yahoo.com>
2022-07-10 16:44:33 -0400

    Add play() function

--- script.js
+++ script.js
@@ -15,7 +15,36 @@ function computerPlay() {
 }
 
 function play(playerSelection, computerSelection) {
+    if (playerSelection.toLowerCase() == computerSelection.toLowerCase() ) {
+        return "It's a tie!";
+    } else if (playerSelection.toLowerCase() == 'rock') {
+        if (computerSelection == 'Paper') {
+            return "You Lose! Paper beats Rock"
+        }
+        if (computerSelection == 'Scissors') {
+            return "You Win! Rock beats Scissors"
+        }
+    } else if (playerSelection.toLowerCase() == 'paper') {
+            if (computerSelection == 'Scissors') {
+                return "You Lose! Scissors beats Paper"
+            }
+            if (computerSelection == 'Rock') {
+                return "You Win! Paper beats Rock"
+            }
+    } else if (playerSelection.toLowerCase() == 'scissors') {
+        if (computerSelection == 'Rock') {
+            return "You Lose! Rock beats Scissors"
+        }
+        if (computerSelection == 'Paper') {
+            return "You Win! Scissors beats Paper"
+        }
+
+    }
+    return -1;
+}
+
+function game() {
     
 }
 
-console.log(computerPlay());
+console.log(play('rock',computerPlay()));
