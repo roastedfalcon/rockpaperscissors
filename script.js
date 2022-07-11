aeae8818af0b91c3b1251776c39019b8ffa0f782
Mark Schlining <maschlining@yahoo.com>
2022-07-11 19:09:10 -0400

    Add 5 game match simulation

--- script.js
+++ script.js
@@ -1,37 +1,42 @@
 
 
-function computerPlay() {
+const computerSelection = () => {
+    //generate computer's selection
     let randomChoice = Math.floor(Math.random()*3);
-    let computerChoice;
     switch (randomChoice) {
         case 0:
-            computerChoice = 'Rock';
+            return 'Rock';
         case 1:
-            computerChoice = 'Paper';
+            return 'Paper';
         case 2:
-            computerChoice = 'Scissors';
+            return'Scissors';
     }
-    return computerChoice;
+    
 }
 
-function play(playerSelection, computerSelection) {
+function playRound(playerSelection, computerSelection) {
+    //return a tie if both are the same
     if (playerSelection.toLowerCase() == computerSelection.toLowerCase() ) {
         return "It's a tie!";
-    } else if (playerSelection.toLowerCase() == 'rock') {
+    } 
+    
+    else if (playerSelection.toLowerCase() == 'rock') {
         if (computerSelection == 'Paper') {
             return "You Lose! Paper beats Rock"
         }
         if (computerSelection == 'Scissors') {
             return "You Win! Rock beats Scissors"
         }
-    } else if (playerSelection.toLowerCase() == 'paper') {
+    } 
+    else if (playerSelection.toLowerCase() == 'paper') {
             if (computerSelection == 'Scissors') {
                 return "You Lose! Scissors beats Paper"
             }
             if (computerSelection == 'Rock') {
                 return "You Win! Paper beats Rock"
             }
-    } else if (playerSelection.toLowerCase() == 'scissors') {
+    } 
+    else if (playerSelection.toLowerCase() == 'scissors') {
         if (computerSelection == 'Rock') {
             return "You Lose! Rock beats Scissors"
         }
@@ -40,11 +45,26 @@ function play(playerSelection, computerSelection) {
         }
 
     }
-    return -1;
+    return;
 }
 
 function game() {
-    
+    //play a series of 5 games
+    let totalScore = 0;
+    for (let i = 0; i < 5; i++) {
+        let playerSelection = prompt("Rock, paper, or scissors?");
+        let roundResult = playRound(playerSelection, computerSelection());
+        console.log(roundResult);
+
+        if (roundResult.includes('You Win!')) totalScore++;
+        if (roundResult.includes('You Lose!')) totalScore--; 
+     }
+
+    console.log(`Total Score: ${totalScore}`);
+
+    if (totalScore > 0) console.log('Congratulations! You are the winner!');
+    else if (totalScore < 0 ) console.log('RIP. You are the loser.');
+    else console.log('Match ended in a tie');
 }
 
-console.log(play('rock',computerPlay()));
+game();
